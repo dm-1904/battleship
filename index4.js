1eb97cf2f6a1da3c54e6a962a2d47458faf98d51
damonryon <damon.ryon@gmail.com>
2024-09-13 11:11:08 -0700

    complete first draft, semi working code

--- index4.js
+++ index4.js
@@ -1,12 +1,13 @@
 const readlineSync = require('readline-sync')
-const youWin = `========
+const youWin = `
+========
 __   _______ _   _   _    _ _____ _   _
-\ \ / /  _  | | | | | |  | |_   _| \ | |
- \ V /| | | | | | | | |  | | | | |  \| |
-  \ / | | | | | | | | |/\| | | | | . ' |
-  | | \ \_/ / |_| | \  /\  /_| |_| |\  |
-  \_/  \___/ \___/   \/  \/ \___/\_| \_/
-========`
+\\ \\ / /  _  | | | | | |  | |_   _| \\ | |
+ \\ V /| | | | | | | | |  | | | | |  \\| |
+  \\ / | | | | | | | | |/\\| | | | | . ' |
+  | | \\ \\_/ / |_| | \\  /\\  /_| |_| |\\  |
+  \\_/  \\___/ \\___/   \\/  \\/ \\___/\\_| \\_/
+========`;
 const board6x6 = [
   [
     { type: "empty", hit: false },
@@ -128,25 +129,19 @@ const smallShipBoard = []
 const bigShipBoard = []
 let board
 const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-// console.table(board)
-// console.log(board[0][0])
-// secondPointCoordinate(4, 5)
-// firstPointCoordinate(4, 5)
-// convertSetsToArrays(point1, point2)
-// smallBoatPositions(startingCoordinates)
+const boardObj = {}
 
-// create a function that can create any size board from an input
-// create a helper that finds a random point on that board
-// places small ship
-// places large ship
-
+/* I had issues with the Count variable on line 295.
+   I tried to make it a global variable and make a starting value in the IF conditions
+   of the selectBoardSize function but it threw and error. So the game ends on the larger boards before it should.
+   Also, when the board is selected, the board with the ship's location is printed.
+   I don't know why either is happening */
 
 
 const firstPointCoordinate = (quantity, max) => {
   while (point1.size !== quantity) {
     point1.add(Math.floor(Math.random() * max))
   }
-  console.log(point1)
   return point1
 }
 
@@ -154,7 +149,6 @@ const secondPointCoordinate = (quantity, max) => {
   while (point2.size !== quantity) {
     point2.add(Math.floor(Math.random() * max))
   }
-  console.log(point2)
   return point2
 }
 
@@ -164,12 +158,10 @@ const convertSetsToArrays = (set1, set2) => {
   for (let i = 0; i < set1.length && set2.length; i++) {
     startingCoordinates.push([set1[i], set2[i]])
   }
-  console.log(startingCoordinates)
   return startingCoordinates
 }
 
 const smallBoatPositions = (arr, max, min) => {
-  console.log(max)
   for (let i = 0; i < arr.length-min; i++) {
     smallShipBoard.push(arr[i])
     if (arr[i][0] === max) {
@@ -178,7 +170,7 @@ const smallBoatPositions = (arr, max, min) => {
       smallShipBoard.push([arr[i][0]+1, arr[i][1]])
     }
   }
-  return //bigBoatPositions(startingCoordinates)
+  return
 }
 
 const bigBoatPositions = (arr, min) => {
@@ -192,34 +184,19 @@ const bigBoatPositions = (arr, min) => {
       bigShipBoard.push([arr[i][0]+2, arr[i][1]])
     }
   }
-  // console.log(smallShipBoard)
-  // console.table(smallShipBoard)
-  // console.log(bigShipBoard)
-  // console.table(bigShipBoard)
   return battleShipPlacer(smallShipBoard, bigShipBoard)
 }
 
 
 
 const battleShipPlacer = (smallArr, bigArr) => {
-  // console.log("smallArr: ", smallArr);
-  // console.log("bigArr: ", bigArr);
   for (let i = 0; i < smallArr.length; i++) {
-    // console.log("smallArr elements: ", smallArr[i][0], smallArr[i][1]);
     let smallBoatCoordinate1 = smallArr[i][0]
     let smallBoatCoordinate2 = smallArr[i][1]
-
-    // console.log("board position: ", board[smallBoatCoordinate1][smallBoatCoordinate2]);
     let smallBoatObj = board[smallBoatCoordinate1][smallBoatCoordinate2]
     smallBoatObj.type = 'small'
     smallBoatObj.id = 2
-    // console.log(smallBoatCoordinate2)
-    // console.log(smallArr[i][0], smallArr[i][1])
-    // console.log(board[smallBoatCoordinate1][smallBoatCoordinate2])
-    // console.log(board[0][0])
-    // console.log(smallBoatObj)
   }
-  // console.log(bigArr)
   for (let i = 0; i < bigArr.length; i++) {
     let bigBoatCoordinate1 = bigArr[i][0]
     let bigBoatCoordinate2 = bigArr[i][1]
@@ -227,13 +204,12 @@ const battleShipPlacer = (smallArr, bigArr) => {
     bigBoatObj.type = 'large'
     bigBoatObj.id = 1
   }
-  // console.table(printBoard(board, true))
   return printBoard(board, true)
 }
 
 
 const printBoard = (board, debug) => {
-  const boardObj = {}
+
   const boardRows = 'ABCDEF'
   if (debug === false) {
     for(let i = 0; i < board.length; i++) {
@@ -297,6 +273,7 @@ const selectBoardSize = () => {
     convertSetsToArrays(point1, point2)
     smallBoatPositions(startingCoordinates, 5, 2)
     bigBoatPositions(startingCoordinates, 2)
+    takeYourShot(board)
   }
   if (userAnswer === 1) {
     board = board5x5
@@ -305,6 +282,7 @@ const selectBoardSize = () => {
     convertSetsToArrays(point1, point2)
     smallBoatPositions(startingCoordinates, 4, 1)
     bigBoatPositions(startingCoordinates, 2)
+    takeYourShot(board)
   }
   if (userAnswer === 0) {
     board = board4x4
@@ -313,45 +291,29 @@ const selectBoardSize = () => {
     convertSetsToArrays(point1, point2)
     smallBoatPositions(startingCoordinates, 3, 1)
     bigBoatPositions(startingCoordinates, 1)
+    takeYourShot(board)
   }
 }
 
-const takeYourShot = () => {
+const takeYourShot = (arg) => {
   let count = 0
-  console.table(boardObj)
-  for (let i = 0; i < testBoard1.length; i++) {
-    // console.log(el)
-    for (let j = 0; j < testBoard1[i].length; j++) {
-      // console.log(testBoard1[i][j].hit)
-      if (testBoard1[i][j].id !== undefined && testBoard1[i][j].hit === true) count++
-
+  for (let i = 0; i < arg.length; i++) {
+    for (let j = 0; j < arg[i].length; j++) {
+      if (arg[i][j].id !== undefined && arg[i][j].hit === true) count++
     }
   }
   if (count === 5) {
-    console.log('YOU WIN!!!')
+    console.log(youWin)
     return
   }
-  // console.log(count)
   const shot = readlineSync.question('Enter a location to strike: ').toUpperCase()
-  // let letter
-  // let number
-  // let letterToIndex
   let letter = shot[0]
   let number = shot[1]
   let letterToIndex = alphabet.indexOf(letter)
-  testBoard1[letterToIndex][number].hit = true
-  // console.log(testBoard1[letterToIndex][number].hit)
-  printBoard(testBoard1, false)
-  // console.table(boardObj)
-  // console.log(testBoard1)
-  takeYourShot()
-  // console.log(letterToIndex)
-  // console.log(boardObj[letter])
-  // console.log(boardObj[letter][number])
-  // // console.log(letter, number)
-  // console.log(boardObj)
-  // console.table(boardObj)
+  arg[letterToIndex][number].hit = true
+  printBoard(arg, false)
+  takeYourShot(arg)
 }
 
-takeYourShot()
+
 greetUser()
